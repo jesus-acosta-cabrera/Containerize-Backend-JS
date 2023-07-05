a5606f68707569203c8d303b01186b4ac3f463b5
Jesus Acosta <jesus_acosta05@hotmail.com>
2023-07-05 11:56:23 -0400

    Seccion

--- API/seccion.js
+++ API/seccion.js
@@ -4,16 +4,19 @@ const router = exp.Router();
 
 // router.post('/Insertar', (req, res) =>)
 
-router.post('/Horario', (req, res) => {
+router.post('/Horario/Asignar', (req, res) => {
     metodo.asignarHorario(req.body.horario, req.body.seccion, res);
 });
 
-router.patch('/Horario', (req, res) => {
+router.patch('/Horario/Modificar', (req, res) => {
     metodo.Mseccion(req.body.seccionID,req.body.AhorarioID,req.body.BhorarioID, res);
 });
 
-router.delete('/Horario', (req, res) => {
+router.delete('/Eliminar', (req, res) => {
     metodo.Eseccion(req.body.ID, res);
 });
 
+router.post('/Crear', (req, res ) => {
+    metodo.Iseccion (req.body.numero, req.body.periodo, req.body.profesor, req.body.codigoA, res);
+})
 module.exports = router;
